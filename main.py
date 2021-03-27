078aaa24331af22e91548d4d146b15a8ab50190b
joe <joesho112358@gmail.com>
2021-03-26 21:09:05 -0400

    updating batteryVolt name to match the html form and typing to int from string and making totalrpm a decimal

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         user_kv = request.form['motorkV']
-        user_batteryVolt = request.form['batteryVolt']
-        user_pinion = request.form['pinion']
-        user_spur = request.form['spur']
-        user_fgr = request.form['fgr']
-        user_wheelradius = request.form['wheelradius']
-        totalrpm = user_kv*user_batteryVolt
-        speed = (totalrpm/((user_spur/user_pinion)*user_fgr)*user_wheelradius*(60/5280))
+        user_batteryVolt = int(request.form['battVolt'])
+        user_pinion = int(request.form['pinion'])
+        user_spur = int(request.form['spur'])
+        user_fgr = int(request.form['fgr'])
+        user_wheelradius = int(request.form['wheelradius'])
+        totalrpm = user_kv * user_batteryVolt
+        totalrpm_float = float(totalrpm)
+        speed = (totalrpm_float / ((user_spur / user_pinion) * user_fgr) * user_wheelradius * (60 / 5280))
         return render_template("index.html", speed_display=speed)
 
 
     else:
         return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
