87b2e14dc1a6289bc57c8bf86cdef1523da4f384
joe <joesho112358@gmail.com>
2021-03-27 00:12:57 -0400

    updating type conversions and allowing html to have a decimal in some fields

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        user_kv = request.form['motorkV']
-        user_batteryVolt = int(request.form['battVolt'])
+        user_kv = int(request.form['motorkV'])
+        user_batteryVolt = float(request.form['battVolt'])
         user_pinion = int(request.form['pinion'])
         user_spur = int(request.form['spur'])
-        user_fgr = int(request.form['fgr'])
-        user_wheelradius = int(request.form['wheelradius'])
+        user_fgr = float(request.form['fgr'])
+        user_wheelradius = float(request.form['wheelradius'])
         totalrpm = user_kv * user_batteryVolt
-        totalrpm_float = float(totalrpm)
-        speed = (totalrpm_float / ((user_spur / user_pinion) * user_fgr) * user_wheelradius * (60 / 5280))
+        speed = (totalrpm / ((user_spur / user_pinion) * user_fgr) * user_wheelradius * (60 / 5280))
         return render_template("index.html", speed_display=speed)
 
 
