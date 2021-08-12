62e5b923661d1bc44825f8cec07f67477f5884b8
cbjfan2009 <cbjfan2009@gmail.com>
2021-08-12 14:18:05 -0400

    added class declaration for SQLAlchemy ORM build

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request
 from math import pi
-# import mysql.connector
 import psycopg2
+from sqlalchemy import create_engine, Table, Column, Integer, String, DECIMAL, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# mysql connection -- if MySQL server is going to be used
+# ---------------------------------mysql connection -- if MySQL server is going to be used-----------------------------
+
+# import mysql.connector
 # sqldb = mysql.connector.connect(
 #   host='localhost',
 #   user='devMatt',
 #   password='D3V3l0pm3ntS3rV3rcbjfan2009',
 #   database='webapp_user_input'
 #   )
-
 
 # mysql query
 # def sql_query():
@@ -20,46 +23,93 @@ import psycopg2
 #    return sql_data
 
 
-# postgresql connection -- if PostgreSQL is going to be used, like with Heroku!
+# ------------------postgresql connection -- if PostgreSQL is going to be used, like with Heroku!-----------------------
+#import psycopg2
 # postgres://vlinujzpemehpy:81bb09e53a2532c52b6a8696ebf0497b253e0a0516f0c1c741d5ceae3e10806e@ec2-54-211-160-34.compute-1.amazonaws.com:5432/df5p5d20v6pbf9
-pgdb = psycopg2.connect(
-   host='ec2-54-211-160-34.compute-1.amazonaws.com',
-   user='vlinujzpemehpy',
-   password='81bb09e53a2532c52b6a8696ebf0497b253e0a0516f0c1c741d5ceae3e10806e',
-   port='5432',
-   database='df5p5d20v6pbf9'
-   )
-pgcursor = pgdb.cursor()
+#pgdb = psycopg2.connect(
+#   host='ec2-54-211-160-34.compute-1.amazonaws.com',
+#   user='vlinujzpemehpy',
+#   password='81bb09e53a2532c52b6a8696ebf0497b253e0a0516f0c1c741d5ceae3e10806e',
+#   port='5432',
+#   database='df5p5d20v6pbf9'
+#   )
+#pgcursor = pgdb.cursor()
 
 
-def check_connection():
-   pgcursor.execute("select version()")
-   data = pgcursor.fetchone()
-   print("Connection established to: ", data)
+#def check_connection():
+#   pgcursor.execute("select version()")
+#   data = pgcursor.fetchone()
+#   print("Connection established to: ", data)
 
 
-def create_table():
-   sql = '''CREATE TABLE estimation_data 
-   (pkid text,
-    Motor_kV integer,
-     Batt_Volt decimal,
-      Pinion integer,
-       Spur integer,
-        Final_Ratio decimal,
-         Wheel_Rad decimal);'''
-   pgcursor.execute(sql)
-   pgdb.commit()
+#def create_table():
+#   sql = '''CREATE TABLE estimation_data
+#   (pkid text,
+#    Motor_kV integer,
+#     Batt_Volt decimal,
+#      Pinion integer,
+#       Spur integer,
+#        Final_Ratio decimal,
+#         Wheel_Rad decimal);'''
+#   pgcursor.execute(sql)
+#   pgdb.commit()
 
 
-def pg_add_data(sql, val):
-   pgcursor.execute(sql, val)
-   pgdb.commit()
+#def pg_add_data(sql, val):
+#   pgcursor.execute(sql, val)
+#   pgdb.commit()
 
 
-def pg_query():
-    pgcursor.execute('SELECT * FROM estimation_data')
-    sql_data = pgcursor.fetchall()
-    return sql_data
+#def pg_query():
+#    pgcursor.execute('SELECT * FROM estimation_data')
+#    sql_data = pgcursor.fetchall()
+#    return sql_data
+
+
+
+# -------------------------------------SQLAlchemy approach-----------------------------------------------
+db_string = "postgresql://vlinujzpemehpy:81bb09e53a2532c52b6a8696ebf0497b253e0a0516f0c1c741d5ceae3e10806e@ec2-54-211-160-34.compute-1.amazonaws.com:5432/df5p5d20v6pbf9"
+db = create_engine(db_string, echo=True)
+meta = MetaData()
+base = declarative_base()
+
+
+class Visitors(base):
+    __tablename__ = 'estimation_data'
+
+    pkid = Column(String, primary_key=True)
+    motor_kV = Column(Integer)
+    batt_Volt = Column(DECIMAL)
+    pinion = Column(Integer)
+    spur = Column(Integer)
+    fgr = Column(DECIMAL)
+    wheel_rad = Column(DECIMAL)
+
+Session = sessionmaker(db)
+session = Session()
+
+base.metadata.create_all(db)
+
+
+#---------------------sample code for CRUD not formatted to work on my db yet------------------------------------------
+# Create
+doctor_strange = Film(title="Doctor Strange", director="Scott Derrickson", year="2016")
+session.add(doctor_strange)
+session.commit()
+
+# Read
+films = session.query(Film)
+for film in films:
+    print(film.title)
+
+# Update
+doctor_strange.title = "Some2016Film"
+session.commit()
+
+# Delete
+session.delete(doctor_strange)
+session.commit()
+#------------------------------------------------end sample code---------------------
 
 
 app = Flask(__name__)
@@ -83,7 +133,7 @@ def index():
             (Motor_kV, Batt_Volt, Pinion, Spur, Final_Ratio, Wheel_Rad)
             VALUES (%s, %s, %s, %s, %s, %s);'''
         y = (user_kv, user_batteryVolt, user_pinion, user_spur, user_fgr, user_wheelradius)
-        pg_add_data(x, y)
+        #pg_add_data(x, y)
         return render_template("index.html", speed_display=speed)
 
     else:
@@ -103,9 +153,12 @@ def poll():
 @app.route('/estimator_data', methods=['POST', 'GET'])
 def estimator_data():
     try:
-        check_connection()
-        pgdata = pg_query()
-        return render_template("estimator_data.html", sql_data=pgdata)
+        result_set = db.execute("SELECT * FROM estimation_data")
+
+       #----postgres-----
+       # check_connection()
+       # pgdata = pg_query()
+        return render_template("estimator_data.html", sql_data=result_set)
     except:
         return "Something went wrong with the database connection!"
     #sqldata = sql_query() <--if using mysql
